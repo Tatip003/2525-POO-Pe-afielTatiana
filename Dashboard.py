6258951a2150f951d74e7412bb92449ee059bbd6
Tatip003 <ts.penafielm@uea.edu.ec>
2025-07-18 18:28:08 -0500

    Tarea semana 08

---
+++ Dashboard.py
@@ -0,0 +1,47 @@
+import os
+
+def mostrar_codigo(ruta_script):
+    # Asegúrate de que la ruta al script es absoluta
+    ruta_script_absoluta = os.path.abspath(ruta_script)
+    try:
+        with open(ruta_script_absoluta, 'r') as archivo:
+            print(f"\n--- Código de {ruta_script} ---\n")
+            print(archivo.read())
+    except FileNotFoundError:
+        print("El archivo no se encontró.")
+    except Exception as e:
+        print(f"Ocurrió un error al leer el archivo: {e}")
+
+def mostrar_menu():
+    # Define la ruta base donde se encuentra el dashboard.py
+    ruta_base = os.path.dirname(__file__)
+
+    opciones = {
+        '1': 'Parcial 01/Semana 02/Técnicas de Programación.py',
+        '2': 'Parcial 01/Semana 03/Programacion Orientada a Objetos.py',
+        '3': 'Parcial 01/Semana 04/EjemplosMundoReal_POO.py',
+        '4': 'Parcial 01/Semana 05/Area de un circulo.py',
+        '5': 'Parcial 01/Semana 06/Programa cafetería.py',
+        '6': 'Parcial 01/Semana 07/Calculadora.py'
+    }
+    while True:
+        print("\nMenu Principal - Dashboard")
+        # Imprime las opciones del menú
+        for key in opciones:
+            print(f"{key} - {opciones[key]}")
+        print("0 - Salir")
+
+        eleccion = input("Elige un script para ver su código o '0' para salir: ")
+        if eleccion == '0':
+            break
+        elif eleccion in opciones:
+            # Asegura que el path sea absoluto
+            ruta_script = os.path.join(ruta_base, opciones[eleccion])
+            mostrar_codigo(ruta_script)
+        else:
+            print("Opción no válida. Por favor, intenta de nuevo.")
+
+
+# Ejecutar el dashboard
+if __name__ == "__main__":
+    mostrar_menu()
